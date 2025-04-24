823491edb8c1377c71ee2e21856d3ded2d0176e3
LebowskiDude <tim.lightbourne@gmail.com>
2025-04-24 18:09:40 +0100

    [Learn React]-[Static Pages]-[React Facts App]

---
+++ src/ReactFactsApp.jsx
@@ -0,0 +1,15 @@
+function ReactFactsApp() {
+    return (
+        <>  
+            <img src="/src/assets/react.svg" alt="React Logo" width="40px"/>
+            <h1>Fun facts about React!</h1>
+            <ul>
+                <li>It's maintained by Meta</li>
+                <li>It has well over 100k stars on GitHub</li>
+            </ul>
+        </>
+
+    )
+}
+
+export default ReactFactsApp;
