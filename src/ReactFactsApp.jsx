3d98d50c466d1a74ba6f57d80ac770c090484218
LebowskiDude <tim.lightbourne@gmail.com>
2025-04-25 13:36:20 +0100

    Updating Project - code sections placed into spearate Component functions.

--- src/ReactFactsApp.jsx
+++ src/ReactFactsApp.jsx
@@ -1,14 +1,45 @@
+function Header() {
+    return (
+        <header className="header">
+            <img src="/src/assets/react.svg" alt="React logo" className="header-logo" />
+            <nav>
+                <ul className="nav-list">
+                    <li className="nav-list-item">Pricing</li>
+                    <li className="nav-list-item">About</li>
+                    <li className="nav-list-item">Contact</li>
+                </ul>
+            </nav>
+        </header>
+    )
+}
+
+function MainContent() {
+    return (
+        <main>
+            <h1>Reason I am excited to learn React</h1>
+            <ol>
+                <li>React is a popular library, so I will be able to fit in with all the coolest devs out there! 😎</li>
+                <li>I am more likely to get a job as a front end developer if I know React</li>
+            </ol>
+        </main>
+    )
+}
+
+function Footer() {
+    return (
+        <footer>
+            <small>© 2024 Ziroll development. All rights reserved.</small>
+        </footer>
+    )
+}
+
 function ReactFactsApp() {
     return (
         <>  
-            <img src="/src/assets/react.svg" alt="React Logo" width="40px"/>
-            <h1>Fun facts about React!</h1>
-            <ul>
-                <li>It's maintained by Meta</li>
-                <li>It has well over 100k stars on GitHub</li>
-            </ul>
+            <Header />
+            <MainContent />
+            <Footer />
         </>
-
     )
 }
 
