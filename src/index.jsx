2e5ca3b90465fe7ab5941fa3ecafbaf38cbc3bad
LebowskiDude <tim.lightbourne@gmail.com>
2025-04-25 11:36:37 +0100

    Updating Project - renamng main.jsx to index.jsx.

---
+++ src/index.jsx
@@ -0,0 +1,13 @@
+import { StrictMode } from "react"
+import { createRoot } from "react-dom/client"
+import './index.css'
+// import App from './App.jsx' // - demonstration code for application
+import ReactFactsApp from './ReactFactsApp.jsx'
+
+const root = createRoot(document.getElementById("root"));
+root.render(
+  <StrictMode>
+    <ReactFactsApp />
+  </StrictMode>
+)
+
